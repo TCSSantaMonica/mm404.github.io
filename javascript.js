382c748459f96ac3f1db3151e42166ee8b361b06
TCSSantaMonica <115374860+TCSSantaMonica@users.noreply.github.com>
2025-09-27 13:56:50 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,4 +5,9 @@ function createpost() {
   place.appendChild(p)
 }
 
+fetch('levels.json')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("we kinda messed up on teh json: ", error));
+
 Document.getElementById("createpostbutton").onclick = createpost;
